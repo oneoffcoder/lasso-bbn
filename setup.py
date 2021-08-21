4e071a0280216653e9a69a2d11ccb5eee1264d7a
Jee Vang, Ph.D <vangjee@gmail.com>
2021-08-20 20:54:38 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as fh:
 
 setup(
     name='lassobbn',
-    version='0.0.1',
+    version='0.0.2',
     author='Jee Vang',
     author_email='g@oneoffcoder.com',
     packages=find_packages(exclude=('*.tests', '*.tests.*', 'tests.*', 'tests')),
